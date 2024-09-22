993370b8b5de405591ad6159ddfe3be241bd5764
jcjean <jeancharlessjesus@gmail.com>
2024-09-22 19:53:54 -0300

    Exemplo de uma classe em java e seus métodos

---
+++ primeiro-projeto/src/MyClass.java
@@ -0,0 +1,27 @@
+public class MyClass{
+    public double somar(int num1, int num2){
+        int num3 = num1+num2;
+        return num3;
+        //return num1+num2;
+    }
+
+    public void imprimir(String texto){
+        //Logica
+        //Neste caso naão precisa do return pois não será retornado nenhum resultado
+    }
+
+    //throws Exception: indica que o método, ao ser utilizado, poderá gerar uma exceção.
+    public double dividir(int dividendo, int divisor) throws Exception{
+        return dividendo/divisor;
+    }
+
+    //este método não estará visivel para outras classes.
+    private void metodoPrivado(){
+
+    }
+
+    public void gravarCliente(String nome, String cpf, Integer idade){
+        //este método tem a finalidade de gravar informações de um cliente.
+        //porque não criar um objeto cliente e passar como parâmetro?
+    }
+}
