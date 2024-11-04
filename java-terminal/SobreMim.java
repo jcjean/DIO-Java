2aa24a1ba39432dc5d1a83a87a714987d8dfe4db
jcjean <jeancharlessjesus@gmail.com>
2024-11-04 19:36:34 -0300

    Passando parametros por meio de argumentos na execução do progama (direto no terminal)

---
+++ java-terminal/SobreMim.java
@@ -0,0 +1,11 @@
+public class SobreMim{
+    public static void main(String[] args){
+        String nome = args[0];
+        String sobrenome = args[1];
+        String idade = args[2];
+        String altura = args[3];
+
+        System.out.println("Ola, meu nome é "+nome+" "+sobrenome);
+        System.out.println("Tenho "+idade+" anos e "+altura+"cm de altura.");
+    }
+}
