d1e96f0da7e1c787cd141a6f59f080d200a01b4d
jcjean <jeancharlessjesus@gmail.com>
2024-11-04 19:04:29 -0300

    Criando o método soma para testar a criação de um javadoc

---
+++ primeiro-projeto/src/Soma.java
@@ -0,0 +1,18 @@
+/** testando o uso de java doc
+    @author jcjean
+    @version 1.0
+    @since 04/11/2024
+*/
+
+public class Soma {
+    /**
+     * 
+     * @param num1 primeiro parametro do método
+     * @param num2 segundo parametro do método
+     * @return o resultado deste método é a soma dos dois números
+     */
+    public int somar(int num1, int num2){
+        return (num1+num2);
+    }
+}
+//basta usar o comando javadoc Soma.java para geram um html mas essa bomba nao funciona no vscode pelo visto.
